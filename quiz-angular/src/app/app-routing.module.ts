630b914931f3189d3a6864eb462470540f0d0fb0
Victoriia Yemelianova <emelyanovavikaa@gmail.com>
2021-02-04 17:01:30 +0200

    careate quiz component

--- quiz-angular/src/app/app-routing.module.ts
+++ quiz-angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'quiz/:name', component: QuizComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
